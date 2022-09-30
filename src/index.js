a16bb78c13ff311256774d6d659cf15e0a3f81e3
AlvaroMarin-D <alvaromarin.d@gmail.com>
2022-09-29 20:09:37 -0400

    app update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
+import { registerLicense } from '@syncfusion/ej2-base';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+registerLicense(process.env.REACT_APP_SYNC_KEY);
+
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
+
